1f25236498fad9acfeb2c5e7e0278dd837e32710
Camille <hocl@itu.dk>
2025-09-03 13:37:40 +0200

    Update Program.cs

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+//Filen skal indlæses så vi kan håndtere dataen, bruges SR til.
+StreamReader sr = new StreamReader("chirp_cli_db.csv");
+
+//Lave exception til file not found
+
+//Læs hver linje, hvis næste ikke er null
+
+string text = sr.ReadToEnd();
+Console.WriteLine(text);
+
+//Regex brugernavn = new Regex("[a-z]{4,5})");
+// (?<brugernavn>[a-z]{4,5}),"(?<kommentar>[A-Za-z ,!:).]*)",(?<epoch timestamp>\d*) 
