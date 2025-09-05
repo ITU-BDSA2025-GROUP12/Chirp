15f80870a3fb0935ee2f08eb69e49c909ed33a69
Freja <frsj@itu.dk>
2025-09-05 12:17:21 +0200

    Move SimpleDB out of Chirp.CLI
    Coauthored by: Camilla

--- Chirp.CLI/SimpleDB/IDatabaseRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SimpleDB;
-
-public class IDatabaseRepository
-{
-    
-}
--- Chirp.CLI/obj/Debug/net8.0/Chirp.CLI.AssemblyInfo.cs
+++ Chirp.CLI/obj/Debug/net8.0/Chirp.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59092afc4e081a0defe0dfb426b39117460da7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b8d2e408504682821071012532b457e783be86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
