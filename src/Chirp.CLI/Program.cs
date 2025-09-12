fcc42f25c7eece2ee1e9ad5cf66a23e612216953
diahitu <diah@itu.dk>
2025-09-12 17:57:50 +0200

    refactored the code, to use system.commandline

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -1,4 +1,15 @@
 using Chirp.CLI.Models;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System;
+using System.CommandLine.Invocation;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 
 namespace Chirp.CLI;
 using System;
@@ -10,25 +21,37 @@ using SimpleDB;
 
 public class Program
 {
-    static int Main(string[] args)
+    static int Main (string[] args)
     {
         IDatabaseRepository<Cheep> db = new CSVDatabase<Cheep>();
         UserInterface UI = new UserInterface();
-            
-        if (args[0] == "read")
+        Option<string> cheepOpt = new Option<string>(
+                "--cheep");
+        Option<bool> readOpt = new Option<bool>("--read");
+        readOpt.DefaultValueFactory = _ => true;
+        var rootCommand = new RootCommand();
+        rootCommand.Add(cheepOpt);
+        rootCommand.Add(readOpt);
+        ParseResult parseResult = rootCommand.Parse(args);
+        if (parseResult.Errors.Count == 0)
         {
+            if (parseResult.GetValue(cheepOpt) is string c)
+            {
+                db.Store(new Cheep(Environment.UserName, args[1], DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+            }
+            else if (parseResult.GetValue(readOpt))
+            {
             UserInterface.PrintCheeps(db.Read());
+            }
+            return 0;
         }
-        else if (args[0] == "cheep")
+        else
         {
-            db.Store(new Cheep(Environment.UserName, args[1], DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+                foreach (ParseError parseError in parseResult.Errors)
+                {
+                    Console.WriteLine(parseError.Message);
+                }
+            return 1;
         }
-
-        
-        return 0;
     }
 }
-
-
-    
-    
