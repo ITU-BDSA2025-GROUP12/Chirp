800b19aab60ca7db9e3907a66e5c249ac73b099d
Camilla <cfjo@itu.dk>
2025-09-12 11:20:57 +0200

    added UserInterface class

--- src/Chirp.CLI/Program.cs
+++ src/Chirp.CLI/Program.cs
@@ -7,25 +7,24 @@ using System.Text.RegularExpressions;
 using SimpleDB;
 
 
+
 public class Program
 {
     static int Main(string[] args)
     {
         IDatabaseRepository<Cheep> db = new CSVDatabase<Cheep>();
+        UserInterface UI = new UserInterface();
             
         if (args[0] == "read")
         {
-            foreach (Cheep cheep in db.Read())
-            {
-                var time = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(cheep.Timestamp).ToString("MM/dd/yy hh:mm:ss",CultureInfo.InvariantCulture);
-                Console.WriteLine(cheep.Author + " @ " + time + ": "+ cheep.Message);
-            }
+            UserInterface.PrintCheeps(db.Read());
         }
         else if (args[0] == "cheep")
         {
             db.Store(new Cheep(Environment.UserName, args[1], DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
         }
 
+        
         return 0;
     }
 }
---
+++ src/Chirp.CLI/UserInterface.cs
@@ -0,0 +1,19 @@
+using Chirp.CLI.Models;
+
+namespace Chirp.CLI;
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using SimpleDB;
+
+public class UserInterface
+{
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach (Cheep cheep in cheeps)
+        { 
+            var time = new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(cheep.Timestamp).ToString("MM/dd/yy hh:mm:ss",CultureInfo.InvariantCulture);
+            Console.WriteLine(cheep.Author + " @ " + time + ": "+ cheep.Message);
+        }
+    }
+}
