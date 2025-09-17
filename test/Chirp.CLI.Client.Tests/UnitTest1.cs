7617a9b80bfd3fad06e3ce8f5ebe3563f2aa8c9e
diahitu <diah@itu.dk>
2025-09-17 21:41:35 +0200

    added another unit test to Chrip.CLI.Client.Tests

--- test/Chirp.CLI.Client.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Client.Tests/UnitTest1.cs
@@ -10,4 +10,18 @@ public class UnitTest1
         var result = Chirp.CLI.Program.Main(["--read"]);
         Assert.Equal(0, result);
     }
+
+    [Fact]
+    public void Test2()
+    {
+        StringWriter stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        Chirp.CLI.Program.Main(["--read"]);
+        string actual_output = stringWriter.ToString();
+        string expected_output = "annarasmussen @ 09/07/25 12:19:25: hej gutter\n" +
+            "Freja @ 09/12/25 08:02:53: hej gunter\n" +
+            "ditte @ 09/12/25 03:46:17: hej hej\n" +
+            "ditte @ 09/12/25 03:51:41: hej igen\n";
+        Assert.Equal(expected_output, actual_output);
+    }
 }
