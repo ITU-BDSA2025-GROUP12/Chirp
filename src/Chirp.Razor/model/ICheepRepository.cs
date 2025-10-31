c26cf19adadcea57d2f17c2509d014e1cedc8b45
Camille <hocl@itu.dk>
2025-10-31 11:36:08 +0100

    This better work, added methods in ICheepRepository

--- src/Chirp.Razor/model/ICheepRepository.cs
+++ src/Chirp.Razor/model/ICheepRepository.cs
@@ -2,4 +2,11 @@ public interface ICheepRepository
 {
     public List<Cheep> GetCheeps(int page); // Query
     public List<Cheep> GetCheepsFromAuthor(string author, int page); // Query
+    Task<int> GetCheepCount();
+    Task<int> GetCheepCountFromAuthor(string author);
+    Task<Author?> FindAuthorByName(string name);
+    Task<Author?> FindAuthorByEmail(string email);
+    Task CreateAuthor(Author author);
+    Task CreateCheep(Cheep cheep);
+
 }
