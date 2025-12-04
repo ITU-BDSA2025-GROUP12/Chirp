da72801e046c21bf8c71b18ea87ccedfebff71b5
Freja <frsj@itu.dk>
2025-12-04 12:29:07 +0100

    Download personal data bug fix

--- src/Chirp.Web/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ src/Chirp.Web/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -45,7 +45,7 @@ namespace newAppp.Areas.Identity.Pages.Account.Manage
 
             // Only include personal data for download
             var personalData = new Dictionary<string, string>();
-            var personalDataProps = typeof(IdentityUser).GetProperties().Where(
+            var personalDataProps = typeof(Author).GetProperties().Where(
                             prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
             foreach (var p in personalDataProps)
             {
