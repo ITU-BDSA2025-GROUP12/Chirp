297b5442d5ae9e7822d8b6847b95e1e068a1a4b0
annanassen <annar@itu.dk>
2025-12-04 12:34:50 +0100

    minor change

--- PlaywrightTests/UITests.cs
+++ PlaywrightTests/UITests.cs
@@ -14,7 +14,7 @@ public class UItests : PageTest {
 		await Page.GotoAsync("https://bdsa2025gr12chirprazor-gzh9b7ghhxb0cybn.norwayeast-01.azurewebsites.net/");
 		await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
 		await Page.GetByPlaceholder("name@example.com").ClickAsync();
-		await Page.GetByPlaceholder("name@example.com").FillAsync("testuser@itu.dk");
+		await Page.GetByPlaceholder("name@example.com").FillAsync("test@test.dk");
 		await Page.GetByPlaceholder("password").ClickAsync();
 		await Page.GetByPlaceholder("password").FillAsync("Test1234!");
 		await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
@@ -28,9 +28,9 @@ public class UItests : PageTest {
 		await Page.GotoAsync("https://bdsa2025gr12chirprazor-gzh9b7ghhxb0cybn.norwayeast-01.azurewebsites.net/");
 		await Page.GetByRole(AriaRole.Link, new() { Name = "Register" }).ClickAsync();
 		await Page.GetByPlaceholder("name@example.com").ClickAsync();
-		await Page.GetByPlaceholder("name@example.com").FillAsync("testuser@itu.dk");
+		await Page.GetByPlaceholder("name@example.com").FillAsync("test@test.dk");
 		await Page.GetByPlaceholder("name", new() { Exact = true }).ClickAsync();
-		await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync("testuser");
+		await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync("test");
 		await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
 		await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Test1234!");
 		await Page.GetByLabel("Confirm Password").ClickAsync();
@@ -43,11 +43,11 @@ public class UItests : PageTest {
 		await Page.GotoAsync("https://bdsa2025gr12chirprazor-gzh9b7ghhxb0cybn.norwayeast-01.azurewebsites.net/");
 		await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
 		await Page.GetByPlaceholder("name@example.com").ClickAsync();
-		await Page.GetByPlaceholder("name@example.com").FillAsync("testuser@itu.dk");
+		await Page.GetByPlaceholder("name@example.com").FillAsync("test@test.dk");
 		await Page.GetByPlaceholder("password").ClickAsync();
 		await Page.GetByPlaceholder("password").FillAsync("Test1234!");
 		await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-		await Page.GetByRole(AriaRole.Link, new() { Name = "Logout [testuser]" }).ClickAsync();
+		await Page.GetByRole(AriaRole.Link, new() { Name = "Logout [test1234]" }).ClickAsync();
 		await Expect(Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" })).ToBeVisibleAsync();
 		await Expect(Page.Locator("body")).ToContainTextAsync("Click here to Logout");
 		await Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
