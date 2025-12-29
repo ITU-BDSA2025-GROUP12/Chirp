72538564c028b91e1a757abee267e16074cc47d5
frxsj <frsj@itu.dk>
2025-12-29 14:11:42 +0100

    Remove blank line in Author class
    Removed unnecessary blank line at the beginning of the Author class.

--- src/Chirp.Core1/author.cs
+++ src/Chirp.Core1/author.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 
-
 public class Author : IdentityUser<int>{
 	public String? FirstName { get; set; } = string.Empty;
 	public ICollection<Cheep> Cheeps { get; } = new List<Cheep>();
