e2e341d0a53e40fbc23a813646667c678e2a17ab
Camilla <cfjo@itu.dk>
2025-12-29 13:20:58 +0100

    doc for author class

--- src/Chirp.Core1/author.cs
+++ src/Chirp.Core1/author.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 
 namespace Chirp.Core1;
-
+/// <summary>
+/// Represents a registered user in Chirp!
+/// Implements .NET's IdentityUser
+/// </summary>
 public class Author : IdentityUser<int>{
+	/// <summary>
+	/// Name of the user
+	/// </summary>
 	public String? FirstName { get; set; } = string.Empty;
+	/// <summary>
+	/// A list of every cheep 
+	/// </summary>
 	public ICollection<Cheep> Cheeps { get; } = new List<Cheep>();
 	public bool IsDeleted { get; set; }
 	public string? DisplayName { get; set; }
